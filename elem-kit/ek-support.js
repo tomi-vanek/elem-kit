84db17ffb795a3844c7b95561f7cbf7a0a91b146
tomi vanek <tomi.vanek@gmail.com>
2025-02-10 20:23:39 +0100

    local css variables for button

--- elem-kit/ek-support.js
+++ elem-kit/ek-support.js
@@ -1,16 +1,26 @@
 // Utility function to calculate CSS path for web components
-function getCssPath(scriptUrl, componentName) {
+function getComponentCss(scriptUrl, componentName) {
   // Get the current script URL to determine base path
   // const scriptUrl = import.meta.url || document.currentScript?.src
-  if (!scriptUrl) return `scripts/elem-kit/${componentName}.css`
+  if (!scriptUrl) return `scripts/elem-kit/${componentName}/${componentName}.css`
 
   // Extract the base path from the script URL
   const basePath = scriptUrl.substring(0, scriptUrl.lastIndexOf('/') + 1)
   return `${basePath}${componentName}.css`
 }
 
+function getBaseCss(scriptUrl) {
+  const basePath = scriptUrl.substring(0, scriptUrl.lastIndexOf('/') + 1)
+  return `${basePath}../ek-styles/ek-base.css`
+}
+
 // Utility function to get CSS link element HTML
 export function linkCss(scriptUrl, componentName) {
-  const cssPath = getCssPath(scriptUrl, componentName)
-  return `<link rel="stylesheet" href="${cssPath}">`
+  const baseCss = getBaseCss(scriptUrl)
+  const componentCss = getComponentCss(scriptUrl, componentName)
+
+  return `
+    <link rel="stylesheet" href="${baseCss}"> 
+    <link rel="stylesheet" href="${componentCss}">
+  `
 } 
