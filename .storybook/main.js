5e84b56bd298ad7f6b06003945c3a54b70073a88
tomi vanek <tomi.vanek@gmail.com>
2025-02-12 22:43:52 +0100

    Updates

---
+++ .storybook/main.js
@@ -0,0 +1,13 @@
+/** @type { import('@storybook/web-components-vite').StorybookConfig } */
+const config = {
+    framework: '@storybook/web-components-vite',
+    stories: ['../elem-kit/**/*.stories.js'],
+    addons: [
+        '@storybook/addon-essentials',
+        '@storybook/addon-links',
+    ],
+    core: { disableTelemetry: true, },
+    port: 8800,
+}
+  
+export default config;
